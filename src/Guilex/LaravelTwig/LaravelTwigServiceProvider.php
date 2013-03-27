597a75cfdcfa1960a6c40b83b03e3e16b47c74ab
guilex <dragan.stefanov@gmail.com>
2013-03-28 00:35:44 +0100

    initial

---
+++ src/Guilex/LaravelTwig/LaravelTwigServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Guilex\LaravelTwig;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTwigServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('guilex/laravel-twig');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
